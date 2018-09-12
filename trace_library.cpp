e111d48daf12930f3f5a2a779a7e2738a89f6218
barbercoder <mkc28@sfu.ca>
2018-09-11 17:40:16 -0700

    updated trace library
    added content on to previously declared functions with new input and output. Also placed the initial characters into the the file that is supposed to be open

--- trace_library.cpp
+++ trace_library.cpp
@@ -6,15 +6,43 @@ ofstream fileoutput;
 
 void trace_start(char* filename)
 {
-	fileoutput.open("/*example.txt*/") //replace the example with actual filename
+	filePname = filename;	// this is assuming that the filename is actually comes in all at once
+	string file = filePname;
+	file = file+".txt";
+	fileoutput.open(file); //replace the example with actual filename
+	// may want to check if file is empty and, empty if there is anything in file
+	if (fileoutput.fail())
+	{
+		fileoutput<< "[ ";
+		myfile.close();
+	}
+	else 
+	{
+		cout<< "Unable to open file"
+		myfile.close();
+	}
 }
 
-/*
+
 void trace_end()
 {
-	
+	string file = filePname;
+	file = file + ".txt";
+	fileoutput.open(file, std::ios_base::app);
+	if (fileoutput.fail())
+	{
+		fileoutput<< "]";
+		myfile.close();
+	}
+	else 
+	{
+		cout<< "Unable to open file"
+		myfile.close();
+	}
 }
 
+
+/*
 void trace_event_start(char*  name, char* categories, char* arguments)
 {
 	
