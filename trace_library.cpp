361d3ddad2bbec83e9dc4965b19799d024f89612
barbercoder <mkc28@sfu.ca>
2018-09-11 19:25:46 -0700

    finished additional functions
    fixed syntax to new code, also added substance to trace_event_start/end, will still have to convert all of it at the end to a string in trace_end
    should test code to make sure it outputs the right thing using previous version before merging these branches together

--- trace_library.cpp
+++ trace_library.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stack>
 
 using namespace std;
 
 string filePname; //this will set aside 64 characters for the filename to be set
 ofstream fileoutput;
+
+
+struct singleE
+{
+	char* nameE;
+	char* cat;
+	char ph;
+	int pid;
+	int tid;
+	int ts;
+};
+
+singleE buffer[10000];
+int i = 0;
 
 void trace_start(char* filename)
 {
@@ -16,43 +31,59 @@ void trace_start(char* filename)
 	// may want to check if file is empty and, empty if there is anything in file
 	if (fileoutput.fail())
 	{
-		fileoutput<< "[";
 		fileoutput.close();
+		cout<<"file didn't open properly";
 	}
-	else 
-	{
-		cout<< "Unable to open file";
-		fileoutput.close();
-	}
+	fileoutput<< "[ ";
 }
 
 
 void trace_end()
 {
-    string filetemp;
-	filetemp = filePname + ".txt";
-	fileoutput.open(filetemp, std::ios_base::app);
-	if (fileoutput.fail())
-	{
-		fileoutput<< "]";
-		fileoutput.close();
-	}
-	else 
-	{
-		cout<< "Unable to open file";
-		fileoutput.close();
-	}
+	//will need to iterate through the stack of singleEs to actually make the file content
+	//then output to fileoutput using a while loop as long as buffer not empty
+	// do this later
+	fileoutput<< "]";
+	fileoutput.close();
 }
 
 
-/*
+
 void trace_event_start(char*  name, char* categories, char* arguments)
-{
-	
+{	
+	singleE part;
+	part.nameE= name;
+	part.cat = categories;
+	part.ph = 'B';
+	part.pid = 1;
+	part.tid = 1;
+	part.ts = 5; //will need to retrieve this value from the the computer clock
+	buffer[i] = part;
+	i++;
 }
 
 void trace_event_end(char* arguements)
 {
-	
+	int k = i;
+	while(i != 0)
+	{
+		singleE temp = buffer[i];
+		if (temp.ph == 'E')
+		{
+			i--;
+			continue;
+		}
+		singleE part2;
+    	part2.nameE= temp.nameE;
+    	part2.cat = temp.cat;
+    	part2.ph = 'E';
+	    part2.pid = temp.pid;
+	    part2.tid = temp.tid;
+	    part2.ts = 5; //will need to retrieve this value from the the computer clock
+	    i = k;
+	    buffer[i] = part2;
+	    i++;
+		break;
+	}
+
 }
-*/
