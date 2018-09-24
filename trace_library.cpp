b776ecef7fcfcddbc97baef57a9b0447bcaf405d
barbercoder <mkc28@sfu.ca>
2018-09-23 22:17:59 -0700

    counter function
    Added counter function

--- trace_library.cpp
+++ trace_library.cpp
@@ -3,10 +3,11 @@
 #include <string>
 #include <stack>
 #include <cstring>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 void printsingleE();
-
 ofstream fileoutput;
 //make sure to include the chrono library to allow the program to tell the computer clock
 //also have to research how to use chrono
@@ -18,7 +19,10 @@ struct singleE
 	string ph;
 	int pid;
 	int tid;
-	int ts;
+	unsigned long long ts;
+	string arg;
+	string key;	// this is for the counter key name
+	string value;	//this is for the counter key value
 };
 
 singleE buffer[10000];
@@ -31,9 +35,9 @@ void trace_start(string filename)
 	if (fileoutput.fail())
 	{
 		fileoutput.close();
-		cout<<"file didn't open properly";
+		cout << "file didn't open properly";
 	}
-	fileoutput<< "[ ";
+	fileoutput << "[ ";
 }
 
 
@@ -43,19 +47,20 @@ void trace_end()
 	//then output to fileoutput using a while loop as long as buffer not empty
 	// do this later
 	printsingleE();
-	fileoutput<< "]";
+	fileoutput << "]";
 	fileoutput.close();
 }
 
 void trace_event_start(string  name, string categories, string arguments)
-{	
+{
 	singleE part;
-	part.nameE= name;
+	part.nameE = name;
 	part.cat = categories;
 	part.ph = "B";
 	part.pid = 1;
 	part.tid = 1;
-	part.ts = 5; //will need to retrieve this value from the the computer clock
+	part.ts = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
+	part.arg = arguments;
 	buffer[i] = part;
 	i++;
 }
@@ -63,44 +68,73 @@ void trace_event_start(string  name, string categories, string arguments)
 void trace_event_end(string arguements)
 {
 	singleE part2;
-    part2.ph = "E";
+	part2.ph = "E";
 	part2.pid = 1;
 	part2.tid = 1;
-	part2.ts = 5; //will need to retrieve this value from the the computer clock
+	part2.ts = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
+	part2.arg = arguements;
 	buffer[i] = part2;
 	i++;
+}
+
+void trace_counter(string name, string key1, string value1)
+{
+	singleE part3;
+	part3.ph = "C";
+	part3.pid = 1;
+	part3.tid = 1;
+	part3.ts = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
+	part3.key = key1;
+	part3.value = value1;
 }
 
 void printsingleE()
 {
 	int k = 0;
-	while(k<10000)
+	while (k < 10000)
 	{
 		singleE temp = buffer[k];
-		if(temp.ph == "B")
+		if (temp.ph == "B")
 		{
-			fileoutput<<"{\"name\": \""<<temp.nameE<<"\",\"cat\": \""<<temp.cat<<"\",";
-			fileoutput<<"\"ph\":\""<<temp.ph<<"\", \"pid\":"<<temp.pid<<",";
-			fileoutput<<"\"tid\":"<<temp.tid<<", \"ts\":"<<temp.ts<<"},"<<endl;
+			fileoutput << "{\"name\": \"" << temp.nameE << "\",\"cat\": \"" << temp.cat << "\",";
+			fileoutput << "\"ph\":\"" << temp.ph << "\", \"pid\":" << temp.pid << ",";
+			fileoutput << "\"tid\":" << temp.tid << ", \"ts\":" << temp.ts << ", \"args\":" << temp.arg << "}," << endl;
 		}
-		else if(temp.ph == "E")
+		else if (temp.ph == "C")
 		{
-			fileoutput<<"{\"ph\":\""<<temp.ph<<"\", \"pid\":"<<temp.pid<<",";
-			fileoutput<<"\"tid\":"<<temp.tid<<", \"ts\":"<<temp.ts<<"}";
-			if(k!=i-1)
+			fileoutput << "{\"name\": \"" << temp.nameE << "\",\"cat\": \"" << temp.cat << "\",";
+			fileoutput << "\"ph\":\"" << temp.ph << "\", \"pid\":" << temp.pid << ",";
+			fileoutput << "\"tid\":" << temp.tid << ", \"ts\":" << temp.ts << ", \"args\":{\"" << temp.key << "\": ";
+			fileoutput << temp.value <<"}}" << endl;
+			if (k != i - 1)
 			{
-				fileoutput<<",";
+				fileoutput << ",";
 			}
-			fileoutput<<endl;
+			fileoutput << endl;
 		}
-		k++;
+		else if (temp.ph == "E")
+		{
+			fileoutput << "{\"ph\":\"" << temp.ph << "\", \"pid\":" << temp.pid << ",";
+			fileoutput << "\"tid\":" << temp.tid << ", \"ts\":" << temp.ts << ", \"args\":" << temp.arg <<"}";
+			if (k != i - 1)
+			{
+				fileoutput << ",";
+			}
+			fileoutput << endl;
+		}
+	k++;
 	}
 	i = 0;
+}
+void trace_flush()
+{
+	printsingleE();
 }
 int main()
 {
 	trace_start("output.txt");
 	trace_event_start("Hello", "greeting", "bah humbug");
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	trace_event_end("bah humbug");
 	trace_end();
 	return 0;
